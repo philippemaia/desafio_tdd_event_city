647c03f4554349996f1d1a59d2e8321a3faa8594
Philippe Maia <philippe.m.maia@hotmail.com>
2025-04-24 23:44:33 -0300

    Finalizado o desafio de TDD event city

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,56 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = cityRepository.findAll(Sort.by("name"));
+        return list.stream().map(CityDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        copyDtoToEntity(dto, entity);
+        entity = cityRepository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    private void copyDtoToEntity(CityDTO dto, City entity) {
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!cityRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        try {
+            cityRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+}
